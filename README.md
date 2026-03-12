f5c1ad84b6a8eb001d1d7fbaab35621b8c188538
Luis Mayta <luis.mayta@gmail.com>
2026-03-12 14:47:25 -0500

    Add version to composer.json and enhance README with migration and seeder instructions

--- README.md
+++ README.md
@@ -14,6 +14,20 @@ composer require luiscamp/laravel-peru-ubigeo
 php artisan vendor:publish --tag=ubigeo-config
 ```
 
+## Publish migrations
+
+```bash
+php artisan vendor:publish --tag=ubigeo-migrations
+php artisan migrate
+```
+
+## Publish and run seeder
+
+```bash
+php artisan vendor:publish --tag=ubigeo-seeders
+php artisan db:seed --class=UbigeoSeeder
+```
+
 ## Testing
 
 ```bash
--- src/LaravelPeruUbigeoServiceProvider.php
+++ src/LaravelPeruUbigeoServiceProvider.php
@@ -13,8 +13,18 @@ class LaravelPeruUbigeoServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
         $this->publishes([
             __DIR__ . '/../config/ubigeo.php' => config_path('ubigeo.php'),
         ], 'ubigeo-config');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+        ], 'ubigeo-migrations');
+
+        $this->publishes([
+            __DIR__ . '/Database/Seeders/UbigeoSeeder.php' => database_path('seeders/UbigeoSeeder.php'),
+        ], 'ubigeo-seeders');
     }
 }
